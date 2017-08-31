5cff32e2a9e876de41d4892068d61dce77e202c4
Erin Zhang <erin-z@users.noreply.github.com>
2017-08-31 22:53:27 +1000

    Create nested_list_comprehension.py

---
+++ nested_list_comprehension.py
@@ -0,0 +1,11 @@
+nums = [0, 1, 2, 3, 4, 5]
+
+[num + 1 for num in nums]
+
+
+# Create a 5 x 5 matrix using a list of lists: matrix
+matrix = [[col for col in range(5)] for row in range(5)]
+
+# Print the matrix
+for row in matrix:
+    print(row)
