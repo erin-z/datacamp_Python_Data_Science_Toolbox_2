54d174df3bea68a4ad7a6c3675c2862d90848527
Erin Zhang <erin-z@users.noreply.github.com>
2017-08-31 14:44:13 +1000

    Update 9_using_*_and_zip_to_unzip.py

--- 9_using_*_and_zip_to_unzip.py
+++ 9_using_*_and_zip_to_unzip.py
@@ -14,4 +14,32 @@ Because the previous print() call would have exhausted the elements in z1, recre
 'Unzip' the tuples in z1 by unpacking them into positional arguments using the * operator in a zip() call. Assign the results to result1 and result2, in that order.
 The last print() statements prints the output of comparing result1 to mutants and result2 to powers. Click Submit Answer to see if the unpacked result1 and result2 are equivalent to mutants and powers, respectively.
 '''
+mutants = ['charles xavier',
+ 'bobby drake',
+ 'kurt wagner',
+ 'max eisenhardt',
+ 'kitty pride']      
 
+aliases =  ['prof x', 'iceman', 'nightcrawler', 'magneto', 'shadowcat']
+
+powers = ['telepathy',
+ 'thermokinesis',
+ 'teleportation',
+ 'magnetokinesis',
+ 'intangibility']
+
+# Create a zip object from mutants and powers: z1
+z1 = zip(mutants, powers)
+
+# Print the tuples in z1 by unpacking with *
+print(*z1)
+
+# Re-create a zip object from mutants and powers: z1
+z1 = zip(mutants, powers)
+
+# 'Unzip' the tuples in z1 by unpacking with * and zip(): result1, result2
+result1, result2 = zip(*z1)
+
+# Check if unpacked tuples are equivalent to original tuples
+print(result1 == mutants)
+print(result2 == powers)
